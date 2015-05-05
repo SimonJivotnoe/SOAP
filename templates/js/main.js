c18ae3513d31b2672a4b69569ea08603a69b9d97
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-05 12:58:14 +0300

    Update main.js

--- templates/js/main.js
+++ templates/js/main.js
@@ -2,6 +2,6 @@ $( document ).ready( function ()
 {
     listOfAutos( );
     $('body').on('click', '.product', function(){
-        console.log('ok');
+        console.log($(this).attr('name'));
     })
 });
