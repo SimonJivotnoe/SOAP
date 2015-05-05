e8a0aa402b103681f3d35a3134ad153986a8ec40
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-05 12:53:54 +0300

    Update ajaxQueries.js

--- templates/js/ajaxQueries.js
+++ templates/js/ajaxQueries.js
@@ -5,8 +5,7 @@ function listOfAutos()
         method: 'GET'
     } ).then( function ( data )
     {
-        localStorage['products'] = JSON.stringify(JSON.parse( data ));
-        var objJSON = JSON.parse( localStorage[ 'products' ] );
+        var objJSON = JSON.parse( data );
         $.each(objJSON, function(key, val){
             $.each(val, function(key, val){
                 if ('id' == key) {
@@ -17,11 +16,6 @@ function listOfAutos()
                     $('#selectSearch').append('<option>'+key+'</option>');
                 }
             })
-        })
-        objJSON = JSON.parse( localStorage[ 'products' ] );
-        console.log(objJSON[0]);
-        $.each(objJSON[0], function(key, val){
-                    $('#selectSearch').append('<option>'+val+'</option>');
         })
 
     } )
