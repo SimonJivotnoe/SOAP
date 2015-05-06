68f40c5bfe03546646eb30671c36cb4609dc362c
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-06 12:36:36 +0300

    Update ajaxQueries.js

--- templates/js/ajaxQueries.js
+++ templates/js/ajaxQueries.js
@@ -10,13 +10,14 @@ function listOfAutos()
             $.each(val, function(key, val){
                 if ('id' == key) {
                     $('.content' ).append(
-                        '<div class="col-md-4 col-md-offset-1 well product" name="'+val+'"></div>');
+                        '<div class="col-md-4 col-md-offset-1 well">' +
+                        '<div class="product" name="'+val+'"></div>' +
+                        '<button class="btn btn-danger order" type="button" name="'+val+'">Order</button></div>');
                 } else {
-                    $('.content').children().last().append('<h2>'+val+'</h2>');
+                    $('.content').children().last().find('.product').append('<h2>'+val+'</h2>');
                 }
             })
         })
-
     } )
 }
 
@@ -27,12 +28,68 @@ function details(id)
         method: 'GET'
     } ).then( function ( data )
     {
-        var objJSON = JSON.parse( data );console.log(objJSON);
-        $.each(objJSON, function(key, val){
-            $.each(val, function(key, val){
-
+        var objJSON = JSON.parse( data );
+        if (objJSON.length > 0) {
+        $('.content' ).html('<div class="col-md-4 col-md-offset-1 well">' +
+        '<div class="details"></div>' +
+        '<div><button class="btn btn-danger order" type="button">Order</button></div>' +
+        '</div>');
+            $.each(objJSON, function(key, val){
+                $.each(val, function(key, val){
+                    if ('id' == key) {
+                        $('.order').attr('name', val)
+                    } else {
+                        $('.details' ).append('<p>' + key + ': ' + val + '</p>');
+                    }
+                })
             })
-        })
+        } else {
 
+        }
     } )
+}
+
+function search(searchInput, searchOption)
+{
+    $.ajax( {
+        url   : 'index.php?action=search&searchInput=' + searchInput +
+        '&searchOption=' + searchOption,
+        method: 'GET'
+    } ).then( function ( data )
+    {
+        var objJSON = JSON.parse( data );
+        if (objJSON.length > 0) {
+            $('.content' ).html('');
+            $.each(objJSON, function(key, val){
+                $.each(val, function(key, val){
+                    if ('id' == key) {
+                        $('.content' ).append('<div class="col-md-4 col-md-offset-1 well">' +
+                        '<div class="details"></div>' +
+                        '<div><button class="btn btn-danger order" type="button" name="'+val+'">Order</button></div>' +
+                        '</div>');
+                    } else {
+                        $('.content').children().last().find('.details').append('<p>' + key + ': ' + val + '</p>');
+                    }
+                })
+            })
+        } else {
+
+        }
+    } )
+}
+
+function confirmOrder(name, surname, payment, id){
+    $.ajax( {
+        url   : 'index.php?action=order&name=' + name +
+        '&surname=' + surname + '&payment=' + payment + '&id=' + id,
+        method: 'GET'
+    } ).then( function ( data )
+    {
+        if (null != data) {
+            $('.content' ).html('successful');
+            setTimeout('window.location.href = "/~user1/PHP/SOAP/SOAP/index.php";', 1100);
+        } else {
+
+        }
+    })
 }
