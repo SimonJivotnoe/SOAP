7e5d4a592d6d8b685c96ad904b8cd9d99746e1be
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-06 12:35:22 +0300

    Update main.js

--- templates/js/main.js
+++ templates/js/main.js
@@ -5,4 +5,45 @@ $( document ).ready( function ()
         var id = $(this).attr('name');
         details(id);
     })
+
+    $('body').on('click', '.order', function(){
+        var id = $(this).attr('name');
+        $('.content').html('<div class="col-md-4 col-md-offset-1 well">' +
+        '<div class="confirmOrder">' +
+        '<input type="text" placeholder="name" id="nameO" />' +
+        '<input type="text" placeholder="surname" id="surnameO" />' +
+        '<p>Type of payment<select type="text" placeholder="surname" id="payment">' +
+        '<option value="cash">cash</option>' +
+        '<option value="card">card</option>' +
+        '</select></p>' +
+        '</div>' +
+        '<div><button class="btn btn-danger confirmOrderBtn" type="button" name="'+id+'">Confirm Order</button></div>' +
+        '</div>');
+    })
+
+    $('body').on('click', '.confirmOrderBtn', function(){
+        var name = $('#nameO' ).val();
+        var surname = $('#surnameO' ).val();
+        var payment = $('#payment option:selected' ).text();console.log(payment);
+        var id = $(this).attr('name');console.log(id);
+        if (name.length > 0 && surname.length > 0) {
+            confirmOrder(name, surname, payment, id);
+        } else {
+
+        }
+    });
+
+    $('.baner').on('click', function(){
+        window.location.href = "index.php";
+    })
+    
+    $('.searchBtn').on('click', function(){
+        var searchInput = $('.searchInput').val();
+        var searchOption = $('#selectSearch option:selected' ).text();
+        if (searchInput.length > 0) {
+            search(searchInput, searchOption);
+        } else {
+
+        }
+    })
 });
