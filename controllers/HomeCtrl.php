4bdfefc5ba2b8474d513e3ef50c5a51e812b9c1d
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-06 11:24:15 +0300

    Update HomeCtrl.php

--- controllers/HomeCtrl.php
+++ controllers/HomeCtrl.php
@@ -1,5 +1,4 @@
 <?php
-
 class HomeCtrl
 {
     /**
@@ -8,21 +7,28 @@ class HomeCtrl
     public function __construct()
     {
         $objView = new View();
-       if (isset($_GET['action'])) {
-           $action = $_GET['action'];
-           $obj = new Client();
-           if ('all' == $_GET['action']) {
-           $res = $obj->listOfAuto();
-           } else if('details' == $_GET['action']){
-            $id = $_GET['id'];   
-            $res = $obj->details($id);
-           } else {
-               
-           }
-
-           $objView->printRes($res);
-       } else {
-           $objView->startPage();
-       }
+        if (isset($_GET['action'])) {
+            $action = $_GET['action'];
+            $obj = new Client();
+            if ('all' == $action) {
+                $res = $obj->listOfAuto();
+            } else if('details' == $action){
+                $id = $_GET['id'];
+                $res = $obj->details($id);
+            } else if('search' == $action){
+                $searchInput = $_GET['searchInput'];
+                $searchOption = $_GET['searchOption'];
+                $res = $obj->search($searchInput, $searchOption);
+            } else if('order' == $action){
+                $name = $_GET['name'];
+                $surname = $_GET['surname'];
+                $payment = $_GET['payment'];
+                $id = $_GET['id'];
+                $res = $obj->order($name, $surname, $payment, $id);
+            }
+            $objView->printRes($res);
+        } else {
+            $objView->startPage();
+        }
     }
 }
