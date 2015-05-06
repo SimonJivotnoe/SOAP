439e2017bd0c98f5ac6d6eb4af457adc1f75db7a
SimonJivotnoe <nk271085mam@gmail.com>
2015-05-06 11:27:10 +0300

    Update AutoService.php

--- server/models/AutoService.php
+++ server/models/AutoService.php
@@ -16,30 +16,54 @@ class AutoService{
 		}
 	}
 
-	function details($id){
-		try{
-			$pdo = PDOModel::connect();
-            $res = $pdo ->select("*")
+    function getDetails($id){
+        try{
+            $pdo = PDOModel::connect();
+            $res = $pdo ->select("id, brand, model, year, capacity, color, max_speed, price")
                 ->from("autos")
-                ->where("id = '$id")
+                ->where("id = '$id'")
                 ->exec();
             if (!empty($res)) {
                 return $res;
             }
-		}catch(Exception $e){
-			throw new SoapFault('something wrong', $e->getMessage());
-		}
-	}
+        }catch(Exception $e){
+            throw new SoapFault('something wrong', $e->getMessage());
+        }
+    }
 
-/*	function getNewsCountByCat($cat_id){
-		try{
-			$sql = "SELECT count(*) FROM msgs WHERE category=$cat_id";
-			$result = $this->_db->querySingle($sql);
-			if (!$result) 
-				throw new Exception($this->_db->lastErrorMsg());
-			return $result;
-		}catch(Exception $e){
-			throw new SoapFault('getNewsCountByCat', $e->getMessage());
-		}
-	}*/
+    function getSearch($resArr){
+        $searchInput = $resArr[0];
+        $searchOption = $resArr[1];
+        try{
+            $pdo = PDOModel::connect();
+            $res = $pdo ->select("id, brand, model, year, capacity, color, max_speed, price")
+                ->from("autos")
+                ->where("$searchOption = '$searchInput'")
+                ->exec();
+            if (!empty($res)) {
+                return $res;
+            }
+        }catch(Exception $e){
+            throw new SoapFault('something wrong', $e->getMessage());
+        }
+    }
+
+    function order($resArr){
+        $name = $resArr[0];
+        $surname = $resArr[1];
+        $payment = $resArr[2];
+        $id = $resArr[3];
+        try{
+            $pdo = PDOModel::connect();
+            $res = $pdo->insert("orders")
+                ->fields("name, surname, payment, car_id")
+                ->values("'$name', '$surname', '$payment', $id")
+                ->execInsertWithLastID();
+            if (!empty($res)) {
+                return $res;
+            }
+        }catch(Exception $e){
+            throw new SoapFault('something wrong', $e->getMessage());
+        }
+    }
 }
